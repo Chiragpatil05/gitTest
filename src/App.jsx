d1e7c92399753931e583d0aee6007447a9d71851
Chiragpatil05 <itschiragpatil5@gmail.com>
2024-08-11 14:11:00 +0530

    login page home added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Login from './components/auth/Login'
 import Dashboard from './components/dashboard/Dashboard'
+import Home from './pages/Home/Home'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <div>
        <Dashboard/>
        <Login/>
+       <Home/>
     </div>
   )
 }
